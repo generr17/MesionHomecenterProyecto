42b17bbd95a72acd65531f6eec916e6e2a109a6b
grubira4167 <grubira4167@uta.edu.ec>
2020-10-07 08:43:51 -0500

    header

---
+++ header.php
@@ -0,0 +1,40 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+     <!--<div class="logo mr-auto"><a href="index.html"><img src="https://ferreteriasbolivar.com/assets/img/ferreteria/logo.jpg" alt="" class="img-fluid"></a></div>--> 
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html" class="logo mr-auto"><img src="assets/img/imagenes/meison.png" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li ><a href="index.html">Inicio</a></li>
+          <li><a href="about.html">Nosotros</a></li>
+         
+          <li><a href="trainers.html">Servicios</a></li>
+          
+          <li class="drop-down"><a href="">Productos</a>
+            <ul>
+             
+              <li class="drop-down"><a href="#">Sub 1</a>
+                <ul>
+                  <li><a href="productos.html">Producto1</a></li>
+                  <li><a href="#">Producto1</a></li>
+                  <li><a href="#">Producto2</a></li>
+                  <li><a href="#">Producto3</a></li>
+                  <li><a href="#">Producto4</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Sub 2</a></li>
+              <li><a href="#">Sub 3</a></li>
+              <li><a href="#">Sub 4</a></li>
+            </ul>
+          </li>
+          <li><a href="puntosVenta.html">Punto de Venta</a></li>
+          <li><a href="contact.html">Contáctanos</a></li>
+          <li><a href="#">Graiman</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php ob_start('comprimir_pagina'); ?> 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +6,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>| Meison Homecenter</title>
+  <title>Inicio | Meison Homecenter</title>
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
@@ -32,46 +33,8 @@
 <body>
 
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-
-     <!--<div class="logo mr-auto"><a href="index.html"><img src="https://ferreteriasbolivar.com/assets/img/ferreteria/logo.jpg" alt="" class="img-fluid"></a></div>--> 
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="index.html" class="logo mr-auto"><img src="assets/img/imagenes/meison.png" alt="" class="img-fluid"></a>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li ><a href="index.html">Inicio</a></li>
-          <li><a href="about.html">Nosotros</a></li>
-         
-          <li><a href="trainers.html">Servicios</a></li>
-          
-          <li class="drop-down"><a href="">Productos</a>
-            <ul>
-             
-              <li class="drop-down"><a href="#">Sub 1</a>
-                <ul>
-                  <li><a href="productos.html">Producto1</a></li>
-                  <li><a href="#">Producto1</a></li>
-                  <li><a href="#">Producto2</a></li>
-                  <li><a href="#">Producto3</a></li>
-                  <li><a href="#">Producto4</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Sub 2</a></li>
-              <li><a href="#">Sub 3</a></li>
-              <li><a href="#">Sub 4</a></li>
-            </ul>
-          </li>
-          <li><a href="puntosVenta.html">Punto de Venta</a></li>
-          <li><a href="contact.html">Contáctanos</a></li>
-          <li><a href="#">Graiman</a></li>
-
-        </ul>
-      </nav><!-- .nav-menu -->
-
-    </div>
-  </header><!-- End Header -->
+  <?php include('header.php'); ?>
+  <!-- End Header -->
 
   <!-- ======= Hero Section ======= -->
 
@@ -540,3 +503,11 @@
 </body>
 
 </html>
+<?php
+ob_end_flush(); 
+function comprimir_pagina($buffer) { 
+    $busca = array('/\>[^\S ]+/s','/[^\S ]+\</s','/(\s)+/s'); 
+    $reemplaza = array('>','<','\\1'); 
+    return preg_replace($busca, $reemplaza, $buffer); 
+} 
+?>
